c1cebe8ee1c95160ba8888e60abd395bd5864fb3
Davis SHYAKA <shyakakingdavis@gmail.com>
2022-12-02 23:26:32 +0200

    fix-bug of ui and add local storage functionality

--- ui/admin/pages/blogs/js/addBlog.js
+++ ui/admin/pages/blogs/js/addBlog.js
@@ -8,19 +8,12 @@ const errorTxt = document.querySelectorAll("#errorTxt");
 const articles = document.getElementById("articles");
 const preview = document.getElementById("file-ip-1-preview");
 let blogArray = [];
-// let oldBlog = null;
-// let oldBlog = window.localStorage.getItem("blog");
-// console.log(oldBlog);
-// if (oldBlog.length > 0) {
-//   console.log("Old Blog Data: ");
-//   for (let i = 0; i < oldBlog.length; i++) {
-//     // console.log(oldBlog[i]);
-//   }
-// }
-// if (oldBlog) {
-//   blogArray = oldBlog;
-// }
-
+let oldBlog = JSON.parse(window.localStorage.getItem("blog")) ?? [];
+if (oldBlog.length > 0) {
+  oldBlog?.forEach((item) => {
+    blogArray.push(item);
+  });
+}
 // previewing the cover image within the form
 function showPreview(event) {
   if (event.target.files.length > 0) {
@@ -33,9 +26,48 @@ function showPreview(event) {
 // on submit event
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   // console.log("button clicked");
   if (formValidation()) {
-    blogArray.push(JSON.parse(JSON.stringify(acceptData())));
+    // base data object
+    const data = {
+      id: "",
+      cover: "",
+      title: "",
+      caption: "",
+      author: "",
+      article: "",
+      posted: "",
+      reads: "",
+      comments: [],
+    };
+    // storing the data
+    const acceptData = () => {
+      data.id = Date.now();
+      data.cover = URL.createObjectURL(image.files[0]);
+      data.title = title.value;
+      data.caption = caption.value;
+      data.author = author.value;
+      data.article = article.value;
+
+      // getting today's date
+      // let today = new Date();
+      // let dd = String(today.getDate()).padStart(2, "0");
+      // let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+      // let yyyy = today.getFullYear();
+      // let hr = today.getHours();
+      // let min = today.getMinutes();
+
+      // today = dd + "/" + mm + "/" + yyyy + " - " + hr + ":" + min;
+      let today = Date.now();
+      today = new Date(today).toDateString();
+      data.posted = today;
+      data.reads = 0;
+      data.comments = 0;
+
+      return data;
+    };
+    blogArray.push(acceptData());
     window.localStorage.setItem("blog", JSON.stringify(blogArray));
     title.value = "";
     caption.value = "";
@@ -46,10 +78,8 @@ form.addEventListener("submit", (e) => {
 
     window.location.replace("../../pages/blogs/index.html");
   }
-  // console.log(blogArray);
 });
 
-// let blogData =
 // validate the form
 let formValidation = () => {
   if (
@@ -90,82 +120,4 @@ function showError(id, serial, message, errorTxt) {
 const showSuccess = (id, serial) => {
   errorTxt[serial].innerHTML = "";
   id.style.border = "1px solid #a8eb12";
-};
-
-// base data object
-const data = {
-  cover: "",
-  title: "",
-  caption: "",
-  author: "",
-  article: "",
-  posted: "",
-  reads: "",
-  comments: "",
-};
-
-// storing the data
-const acceptData = () => {
-  data.cover = URL.createObjectURL(image.files[0]);
-  data.title = title.value;
-  data.caption = caption.value;
-  data.author = author.value;
-  data.article = article.value;
-
-  // getting today's date
-  let today = new Date();
-  let dd = String(today.getDate()).padStart(2, "0");
-  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  let yyyy = today.getFullYear();
-  let hr = today.getHours();
-  let min = today.getMinutes();
-
-  today = dd + "/" + mm + "/" + yyyy + " - " + hr + ":" + min;
-
-  data.posted = today;
-  data.reads = 0;
-  data.comments = 0;
-
-  return data;
-};
-
-let createPost = (data) => {
-  const blogData = document.localStorage.getItem("blog");
-  // getting the stored data and inputing it int the table
-  blogData.forEach((data) => {
-    articles.insertAdjacentHTML(
-      "afterbegin",
-      `
-  <tr>
-  <td data-label="Cover">
-    <img src="${data.cover}" alt="" srcset="" />
-  </td>
-  <td data-label="Title">${data.title}</td>
-  <td data-label="Publ. Date">${data.posted}</td>
-  <td data-label="Reads">${data.reads}</td>
-  <td data-label="Action">
-    <div class="actions">
-        <i id="edit" onClick="editPost(this)" class="fa-solid fa-pen-to-square"></i>
-        <i id="delete" onClick="deletePost(this)" class="fa-solid fa-trash"></i
-    </div>
-  </td>
-</tr>
-   
-    `
-    );
-    title.value = "";
-    caption.value = "";
-    author.value = "";
-    article.value = "";
-    image.value = "";
-    preview.style.display = "none";
-  });
-};
-let deletePost = (e) => {
-  e.parentElement.parentElement.parentElement.remove();
-};
-
-let editPost = (e) => {
-  title.value = e.parentElement.previousElementSibling.innerHTML;
-  e.parentElement.parentElement.remove();
 };
