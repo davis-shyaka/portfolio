6b33cf86fbb8cf83ccf012ebf2970e92a871f356
Davis SHYAKA <shyakakingdavis@gmail.com>
2022-12-04 22:27:26 +0200

    add confirm prompt before deleting blog & add object-fit:cover to images

--- ui/admin/pages/blogs/js/index.js
+++ ui/admin/pages/blogs/js/index.js
@@ -18,6 +18,19 @@ blogData.forEach((item) => {
           </a>
               <i id="delete" data-id = ${item.id} class="fa-solid fa-trash delete-blog"></i>
               
+              <div class="global-container" id="global">
+        <div class="window">
+        <p class="head-msg">Are you sure you want to delete "${item.title}"?</p>
+        <p class="main-msg">Title: ${item.title}</p>
+        <p class="main-msg">Caption: ${item.caption}</p>
+        <p class="main-msg">Reads: ${item.reads}</p>
+        <div class="confirm">
+            <button class="cancel-button">CANCEL</button>
+            <button id="confirmed-delete" class="delete-button">DELETE</button>
+        </div>
+        </div>
+        
+    </div>
           </div>
         </td>
       </tr>
@@ -27,22 +40,44 @@ blogData.forEach((item) => {
 });
 
 const deleteButtons = [...document.getElementsByClassName("delete-blog")];
-// console.log(deleteButtons);
+const confirmedDelete = document.getElementById("confirmed-delete");
 deleteButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const deleteID = e.target.dataset.id;
-    deletePost(deleteID);
+    modal();
+    confirmedDelete.addEventListener("click", (e) => {
+      deletePost(deleteID);
+    });
   });
 });
 
 function deletePost(deleteID) {
-  if (confirm("Are you sure?")) {
-    localStorage.setItem(
-      "blog",
-      JSON.stringify(blogData.filter(({ id }) => id != deleteID))
-    );
-    location.reload();
-  }
+  localStorage.setItem(
+    "blog",
+    JSON.stringify(blogData.filter(({ id }) => id != deleteID))
+  );
+  location.reload();
 }
 
 // Confirm Modal
+let del = document.getElementById("delete-button");
+let modalWindow = document.querySelector(".global-container");
+let cancelButton = document.querySelector(".cancel-button");
+
+function modal() {
+  modalWindow.classList.remove("unpop");
+  modalWindow.classList.add("pop");
+}
+
+cancelButton.addEventListener("click", unmodal);
+function unmodal() {
+  modalWindow.classList.remove("pop");
+  modalWindow.classList.add("unpop");
+}
+
+modalWindow.addEventListener("click", outWin);
+function outWin(e) {
+  if (e.target.id === "global") {
+    unmodal();
+  }
+}
